13d4b39c02b5e0916d3230cd0d14d27a315414b1
kulbir singh <kulbir2406@gmail.com>
2021-08-01 14:43:31 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <div className="card__content">
+        <div className="card__front">
+          <h3 className="card__title">the fair</h3>
+          <p className="card__subtitle">time for some fun</p>
+        </div>
+        <div className="card__back">
+          <p className="card__body">
+            I'm a fan of minimlism in my designs, but since I stumbled across
+            transform-style and some of the fun things that it opens up, I've
+            been playing around a lot with some 3D stuff in CSS of late.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
